e69b20cee8896ad110b1e84182a4a47c8f07a465
Harssh07 <121983623+Harssh07@users.noreply.github.com>
2024-01-31 08:36:16 -0500

    changes in style

--- js/main.js
+++ js/main.js
@@ -7,89 +7,59 @@
     function getMovies() {
       fetch(`${baseUrl}?q=thor`)
         .then(response => response.json())
-        .then(function(response) {
-          
-          console.log(response);
-  
-           const movies = response.description;
+        .then(function (response) {
+          const movies = response.description;
           const ul = document.createElement('ul');
           movies.forEach(movie => {
-           // console.log(movie['#TITLE']);
-           // console.log(movie['#IMDB_ID']); // Fixed typo here
-  
             const li = document.createElement('li');
             const a = document.createElement('a');
-            a.textContent = movie['#TITLE']; // Fixed typo here
-            
-            
+            const img = document.createElement('img'); // Add image element
+  
+            a.textContent = movie['#TITLE'];
             a.dataset.review = movie['#IMDB_ID'];
-            
+  
+            // Set up image source dynamically
+            img.src = `images/${movie['#IMDB_ID']}.jpg`; // Assuming your images are named with IMDB_ID
+  
             li.appendChild(a);
-
-
+            li.appendChild(img); // Append the image to the list item
+  
             ul.appendChild(li);
           });
   
           moviebox.appendChild(ul);
         })
-
-.then(function(){
-    const links = document.querySelectorAll('#movie-box li a');
-    links.forEach(link => {
-        link.addEventListener("click", getReview);
-    })
-})
-
-
+        .then(function () {
+          const links = document.querySelectorAll('#movie-box li a');
+          links.forEach(link => {
+            link.addEventListener("click", getReview);
+          })
+        })
         .catch(error => {
-            console.log(error);
-            //ideallt we would write to the dom and let user know something is wrong
+          console.log(error);
         });
     }
   
-
-
-
-
-function getReview (e) {
-   // console.log("getReview Called");
-  // console.log(this.dataset.review);
-  // console.log(e.currentTarget.dataset.review); THERE IS ERROR IN THIS LINE OF CODE
-
-  const reviewID = e.currentTarget.dataset.review;
-
-
-fetch(`${baseUrl}?tt=${reviewID}`)
-
-.then(response => response.json())
-
-.then(function(response){
-
-    
-    reviewCon.innerHTML = "";
-    
-    console.log(response.short.review.reviewBody);
-
-    const template = document.importNode(reviewtemplate.content,true);
-
-    const reviewBody = template.querySelector(".review-description");
-
-reviewBody.innerHTML = response.short.review.reviewBody;
-
-
-reviewCon.appendChild(template); 
-
-})
-
-.catch(error => {
-    console.log(error);
-})
-
-}
-
-
-
-
+    function getReview(e) {
+      const reviewID = e.currentTarget.dataset.review;
+  
+      fetch(`${baseUrl}?tt=${reviewID}`)
+        .then(response => response.json())
+        .then(function (response) {
+          reviewCon.innerHTML = "";
+  
+          const template = document.importNode(reviewtemplate.content, true);
+  
+          const reviewBody = template.querySelector(".review-description");
+          reviewBody.innerHTML = response.short.review.reviewBody;
+  
+          reviewCon.appendChild(template);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  
     getMovies();
   })();
   
